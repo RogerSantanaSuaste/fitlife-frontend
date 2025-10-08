156100d254f83b0ff5cd40902f40d833758498fd
RogerSantanaSuaste <23393243@utcancun.edu.mx>
2025-10-08 13:02:39 -0500

    Conectado servicio de registro a front.

--- src/models/userRegister.ts
+++ src/models/userRegister.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
+    firstName: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
+    lastName: z.string().min(2, "El apellido debe tener al menos 2 caracteres"),
     email: z.string().email("Dirección de correo electrónico no válida"),
     password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
+    gender: z.enum(['masculino', 'femenino', 'otro'], "Género no válido"),
     returnSecureToken: z.boolean().optional(),
 })
 
